15104ef4679fe7a9a5a44cc1a2f5acce6f8c6419
Sibghatullah Mujaddid <fardiaz138@gmail.com>
2019-08-14 23:24:30 +0700

    Fix enter keydown

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -781,6 +781,10 @@
           case 9:
             //  tab
             return this.onTab();
+          case 13:
+            //  enter.prevent
+            e.preventDefault();
+            return this.typeAheadSelect();
         }
       },
 
